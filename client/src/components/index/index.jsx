71d6c86377b0b6277f30870ee580c39ea61b3ca5
HamzaHarrass <hamzaharrass05@gmail.com>
2023-12-11 19:30:19 +0100

    installer package react router rt ajouter un compenent index pour afficher la premier page

---
+++ client/src/components/index/index.jsx
@@ -0,0 +1,13 @@
+import Navbar from '../../assets/views/common/Navbar.jsx'
+import Body from '../../assets/views/common/Body.jsx
+
+const index = () => {
+  return (
+    <>
+     <Navbar></Navbar>  
+    <Body></Body> 
+    </>
+    )
+}
+
+export default index
