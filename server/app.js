38252421149fbb524d9e56ea22bbb43623140842
HamzaHarrass <hamzaharrass05@gmail.com>
2023-12-12 11:49:15 +0100

    installer nodemon et jest / ajouter deux file .env et app.js

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("./config/DBconnection")()
+const app = express();
+// const authRouter = require("./routes/authRouter");
+const cors = require('cors');
+
+
+app.use(express.json());
+require('dotenv').config();
+
+app.use(cors({
+    origin : "http://localhost:5173"
+}))
+app.get('/',(req,res)=>{
+    res.status(200).json({message:"ALL IS GOOD"})
+})
+// app.use("/auth",authRouter);
+
+app.listen(3000,()=>{
+    console.log("IS WORK ");
+});
