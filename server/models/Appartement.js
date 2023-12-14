11d7736718a7a3ebef311bd513652f17f54b1384
HamzaHarrass <hamzaharrass05@gmail.com>
2023-12-14 08:38:36 +0100

    ajouter status sur model de l'appartement

--- server/models/Appartement.js
+++ server/models/Appartement.js
@@ -4,17 +4,23 @@ const Schema = mongoose.Schema;
 
 const appartementSchema = new Schema({
   address: {
-     type: String, 
-     required: true
-     },
+    type: String,
+    required: true,
+  },
   room: {
-     type: Number,
-      required: true 
-    },
+    type: Number,
+    required: true,
+  },
   prix: {
-     type: Number,
-      required: true 
-    },
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['available', 'occupied', 'under maintenance'], 
+    default: 'available', 
+    required: true,
+  },
 });
 
 const Appartement = mongoose.model('Appartement', appartementSchema);
