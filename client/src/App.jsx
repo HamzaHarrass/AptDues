fadf1ee45363c40dc79861108011b11103a5b46e
HamzaHarrass <hamzaharrass05@gmail.com>
2023-12-19 13:55:56 +0100

    ajouter page controller pour afficher un tableaux et ajouter un modele telecharger bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Auth from "./components/Auth/Auth"
 import Appartement from "./components/appartement/appartement"
 import Detail from "./components/detail/detail"
 import Paiement from "./components/Paiement/Paiement"
+import Controller from "./components/controller/controller"
 import { BrowserRouter,Route, Routes } from "react-router-dom"
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="/appartement" element={<Appartement />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/paiement" element={<Paiement />} />
+        <Route path="/controller" element={<Controller />} />
       </Routes>
       </BrowserRouter>
   </React.StrictMode>
--- server/controllers/paiementController.js
+++ server/controllers/paiementController.js
@@ -3,7 +3,6 @@ const Paiement = require('../models/Paiement');
 const createPaiement = async (req, res) => {
   const { appartement, client } = req.body;
   try {
-    appartement.status = 'occupied';
     const newPaiement = new Paiement({ appartement: appartement, client: client });
     const savedPayment = await newPaiement.save();
     res.status(201).json(savedPayment);
